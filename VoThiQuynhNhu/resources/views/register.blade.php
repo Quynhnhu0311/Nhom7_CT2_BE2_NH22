4a04217dc16fac2a13256087930d8f616b8ae81f
Quynhnhu0311 <93631372+Quynhnhu0311@users.noreply.github.com>
2022-03-30 21:14:18 +0700

    Update Register

--- VoThiQuynhNhu/resources/views/register.blade.php
+++ VoThiQuynhNhu/resources/views/register.blade.php
@@ -5,10 +5,30 @@
   <title>Title</title>
 </head>
 <body>
-<form action="{{ url('register')}}" method = "post">
+<form action="/register" method="post">
 @csrf
 Name <input type="text" name="tname"><br>
 <button type="submit">Register</button>
+</form>
+
+<h1>Test Phuong Thuc Store</h1>
+<form action="product" method="post">
+@csrf
+<button type="submit">Store</button>
+</form>
+
+<h1>Test Phuong Thuc Update</h1>
+<form action="product/12" method="post">
+<input type="hidden" name="_method" value="PUT">
+@csrf
+<button type="submit">Update</button>
+</form>
+
+<h1>Test Phuong Thuc Delete</h1>
+<form action="product/12" method="post">
+<input type="hidden" name="_method" value="DELETE">
+@csrf
+<button type="submit">Delete</button>
 </form>
 </div>
 </body>
