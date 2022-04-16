a484c83fdc1c6767b4caa787ca0368d83133c554
Quynhnhu0311 <93631372+Quynhnhu0311@users.noreply.github.com>
2022-04-16 16:15:04 +0700

    Add migration files

---
+++ VoThiQuynhNhu/database/migrations/2022_04_04_092804_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_cart');
+            $table->integer('id_user');
+            $table->integer('id_detail_cart');
+            $table->string('status');
+            $table->double('amount', 15, 8);
+            $table->dateTime('date_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
---
+++ VoThiQuynhNhu/database/migrations/2022_04_04_093550_create__bao_hanh_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBaoHanhTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_bao_hanh', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_SP');
+            $table->dateTime('ngay_BH');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_bao_hanh');
+    }
+}
