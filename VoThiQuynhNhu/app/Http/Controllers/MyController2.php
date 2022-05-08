db1c81712720d843bf08ef593c36a587760d4b69
Quynhnhu0311 <93631372+Quynhnhu0311@users.noreply.github.com>
2022-05-08 09:04:59 +0700

    Update MyController2.php

--- VoThiQuynhNhu/app/Http/Controllers/MyController2.php
+++ VoThiQuynhNhu/app/Http/Controllers/MyController2.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Protype;
+use App\Models\Product;
+use App\Models\Manufacture;
 
 class MyController2 extends Controller
 {
     function index() {
+        $getAllProtype = Protype::orderBy('id','DESC')->get();
         $products = Product::where('id','<',30)->get();
-        $topSell = Product::where('manu_id','<',2)->get();
-        return view('index',['topSelling'=>$topSell],['data'=>$products]);
+        $topSelling = Product::where('manu_id','<',2)->get();
+        return view('index',compact('getAllProtype','products','topSelling'));
     }
 
     function register(Request $request){
@@ -17,8 +21,15 @@ class MyController2 extends Controller
         $name = $request->tname;
         return view('output',['tname'=>$name]);
     }
-    
-    function store() {
-        return view('store');
+
+    function store($id) {
+        //$getAllProtype = Protype::orderBy('id','DESC')->get();
+        $getProductByType = Product::where('type_id',$id)->get();
+        return view('store',compact('getProductByType'));
+    }
+
+    function product($id) {
+        $detailProduct = Product::where('id',$id)->get();
+        return view('product', compact('detailProduct'));
     }
 }
